3bda99f3eb20dcb9bfa18445551e73cfe7095e15
eTeddy <tpetw886@gmail.com>
2023-04-19 00:07:15 -0500

    init vite project

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,14 @@
+export default function Home() {
+  return (
+    <div className="container">
+      <h2>Home</h2>
+      <div className="home">  
+        <p><iframe width="250" height="auto" src="https://www.youtube.com/embed/OMQ2QARHPo0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe></p>
+        <p><iframe width="250" height="auto" src="https://www.youtube.com/embed/M3InbHr0WAc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe></p>
+        <p><iframe width="250" height="auto" src="https://www.youtube.com/embed/cijPd-TXPn4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe></p>
+        <p><iframe width="250" height="auto" src="https://www.youtube.com/embed/rnhoY5Cdmy0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe></p>
+        <p><iframe width="250" height="auto" src="https://www.youtube.com/embed/cWeWnO8XKds" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe></p>
+      </div>      
+    </div>
+  )
+}
